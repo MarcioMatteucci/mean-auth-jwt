38eceed8838c59ab1ef924fd4301a0873bf67276
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-09-29 20:46:53 -0300

    User Schema

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // Register
-router.get('/register', (req, res) => {
-   res.send('REGISTER');
+router.post('/register', (req, res, next) => {
 });
 
 // Authenticate
-router.get('/authenticate', (req, res) => {
+router.post('/authenticate', (req, res, next) => {
    res.send('AUTHENTICATE');
 });
 
 // Profile
-router.get('/profile', (req, res) => {
+router.get('/profile', (req, res, next) => {
    res.send('PROFILE');
-});
-
-// Profile
-router.get('/validate', (req, res) => {
-   res.send('VADILATE');
 });
 
 module.exports = router;
